7406d8f8a6bd4999a8e58145087aaf36b78cccc0
Pablofvb07 <pablovargas44@gmail.com>
2025-07-10 08:38:15 -0500

    "Se creo el LogsViewModel"

--- ExamenVargas/ViewModels/LogsViewModel.cs
+++ ExamenVargas/ViewModels/LogsViewModel.cs
@@ -1,35 +1,52 @@
-﻿using ExamenVargas.Services;
-using System;
-using System.Collections.Generic;
+﻿
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using ExamenVargas.Services;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Microsoft.Maui.Storage;
+using System.IO;
 
-namespace ExamenVargas.ViewModels
+namespace EJEMPLOPROYECTO.ViewModels
 {
-    public class LogsViewModel: INotifyPropertyChanged
+    public class LogsViewModel : INotifyPropertyChanged
     {
         public ObservableCollection<string> Logs { get; set; } = new ObservableCollection<string>();
+
         private readonly LogService _logService;
-        public LogsViewModel(LogService logService) 
+
+        public LogsViewModel(LogService logService)
         {
             _logService = logService;
             CargarLogs();
         }
-        private async void CargarLogs() 
+
+        private async void CargarLogs()
         {
             string path = Path.Combine(FileSystem.AppDataDirectory, "Logs_Vargas.txt");
+
             if (!File.Exists(path))
             {
-                Logs.Add("Archivo de log no existe");
+                Logs.Add("Archivo de log no existe.");
                 return;
             }
+
             var lines = await File.ReadAllLinesAsync(path);
-            if (lines.Length == 0) 
+            if (lines.Length == 0)
             {
-                Logs.Add("El Archivo de Log está vacio.")
+                Logs.Add("El archivo de log está vacío.");
+                return;
+            }
+
+            Logs.Clear();
+            foreach (var log in lines)
+            {
+                Logs.Add(log);
             }
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
