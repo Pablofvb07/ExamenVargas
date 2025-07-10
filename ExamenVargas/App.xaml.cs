cd49c3b80fe044475bc7587228287ffef781935f
Pablofvb07 <pablovargas44@gmail.com>
2025-07-10 09:03:30 -0500

    "Cambios en Apshell y app.xaml.cs"

--- ExamenVargas/App.xaml.cs
+++ ExamenVargas/App.xaml.cs
@@ -1,10 +1,21 @@
-﻿namespace ExamenVargas
+﻿using ExamenVargas;
+using ExamenVargas.Services;
+using System.IO;
+
+namespace EJEMPLOPROYECTO
 {
     public partial class App : Application
     {
+        public static ProyectosDataBase Database;
+        public static LogService LogService;
+
         public App()
         {
             InitializeComponent();
+
+            string dbPath = Path.Combine(FileSystem.AppDataDirectory, "proyectos.db3");
+            Database = new ProyectosDataBase(dbPath);
+            LogService = new LogService("Vargas");
 
             MainPage = new AppShell();
         }
