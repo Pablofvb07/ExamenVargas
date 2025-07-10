f4d02953d24821f1fa105315dcc9edcbbae70f09
Pablofvb07 <pablovargas44@gmail.com>
2025-07-10 07:38:16 -0500

    "Se crearon los Models del ejercicio"

---
+++ ExamenVargas/Models/Proyectos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace ExamenVargas.Models
+{
+    public class Proyectos
+    {
+        public int id { get; set; }
+        public string NombreProyecto { get; set; }
+        public string Responsable { get; set; }
+        public decimal Progreso { get; set; }
+        public int DuracionDias { get;set; }
+    }
+}
